6fb3391ee18bf82e21b075ddeebd5234363115b4
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-06-14 23:35:07 +0100

    feat: overhaul start buffer

--- early-init.el
+++ early-init.el
@@ -4,7 +4,6 @@
 
 ;;; Setup UI
 (tool-bar-mode -1)
-(global-tab-line-mode)
 (menu-bar--display-line-numbers-mode-relative)
 (global-display-line-numbers-mode)
 (global-hl-line-mode)
@@ -13,4 +12,33 @@
 (display-time-mode)
 
 ;;; Custom start buffer
-(setopt initial-buffer-choice (concat user-emacs-directory "start.org"))
+(setopt inhibit-startup-screen t)
+(with-current-buffer (get-buffer-create "*start*")
+  (insert "* Welcome to Unbox-EMC!\n")
+  (insert "If this is your first time booting, you may see some warning/error messages. These (hopefully) shouldn’t be a problem unless they show up again after reboot.\n")
+  (newline)
+  (insert "You can access the user.el file with '<leader> ffe'. This file is for high level configuration, and is intended to be relatively simple and easy to use.\n")
+  (insert "If you want to access the full config, you can use '<leader> ffc'\n")
+  (newline)
+  (insert "** The leader key is most likely set to space.\n")
+  (newline)
+  (insert-button "Open user.el"
+				 'action (lambda (button)
+						   (open-user-file)))
+  (insert "           <leader> ffe\n")
+  (insert-button "Open Full Config"
+				 'action (lambda (button)
+						   (open-emacs-config)))
+  (insert "       <leader> ffc\n")
+  (insert-button "Use the Buffer Menu"
+				 'action (lambda (button)
+						   (buffer-menu)))
+  (insert "    <leader> bm\n")
+  (insert-button "Restart Emacs"
+				 'action (lambda (button)
+						   (restart-emacs)))
+  (insert "          <leader> qr\n")
+  (insert-button "Exit Emacs"
+				 'action (lambda (button)
+						   (kill-emacs)))
+  (goto-char 1))
--- init.el
+++ init.el
@@ -14,4 +14,10 @@
 (load-file (concat user-emacs-directory "/lisp/packages-melpa.el"))
 (load-file (concat user-emacs-directory "/lisp/keymaps.el"))
 
+;;; Run the user function defined in user.el
 (unbox-user-function)
+
+;;; Switch to custom start buffer
+(switch-to-buffer "*start*")
+(global-tab-line-mode) ; For some reason the start buffer doesn’t respect tab-line-mode being enabled in early-init.el, so it’s enabled here instead.
+(olivetti-mode)
