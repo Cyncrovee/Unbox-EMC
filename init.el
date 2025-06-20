9b11662dd823d0765fd7f8def6bebe5b164bacc8
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-06-20 20:45:20 +0100

    docs: add descriptions for unbox variables

--- init.el
+++ init.el
@@ -1,12 +1,18 @@
 ;;; Setup user settings
 (defvar user-boot-theme nil
-  "Unbox-EMC variable: Defines the theme that will automatically enable on boot")
-(defvar unbox-cursor-blink)
-(defvar unbox-completion-provider)
-(defvar unbox-support-rust)
-(defvar unbox-support-common-lisp)
-(defvar unbox-support-python)
-(defvar unbox-support-lua)
+  "Unbox-EMC variable: Defines the theme that will automatically enable on boot.")
+(defvar unbox-cursor-blink nil
+  "Unbox-EMC variable: Enables-disables cursor blinking.")
+(defvar unbox-completion-provider nil
+  "Unbox-EMC variable: Sets the default completion provider (Company or Corfu) for Unbox-EMC")
+(defvar unbox-support-rust nil
+  "Unbox-EMC variable: Enables Unbox-EMC support for the Rust programming language.")
+(defvar unbox-support-common-lisp nil
+  "Unbox-EMC variable: Enables Unbox-EMC support for the Common Lisp programming language.")
+(defvar unbox-support-python nil
+  "Unbox-EMC variable: Enables Unbox-EMC support for the Python programming language.")
+(defvar unbox-support-lua nil
+  "Unbox-EMC variable: Enables Unbox-EMC support for the Lua programming language.")
 (load-file (concat user-emacs-directory "/lisp/user.el"))
 
 ;;; Load elisp files. These files are loaded in a specific order; changing said order could mess up the config. Be careful!
